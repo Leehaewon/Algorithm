102ecdb3b0e5d5b94f58a994498cfb408b011bce
Leehaewon <leehaewon68@gmail.com>
2019-02-13 21:07:29 +0900

    start project

---
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlgorithmPractice
+//
+//  Created by 이해원 on 2019. 2. 13..
+//  Copyright © 2019년 이해원. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
